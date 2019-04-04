b4c80afe2e777f5443f7e3cc3dc39d1df1cb43a6
houzetorlane <orlanehzt@gmail.com>
2019-04-04 19:47:22 +0200

    Mise à jour du catalogue + index

--- LogSys/index.php
+++ LogSys/index.php
@@ -9,6 +9,67 @@
                 echo '<p>Vous êtes connecté</p>';
             }
         ?>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+        <style>
+
+            .carousel-inner img {
+                width: 100%; /* Set width to 100% */
+                margin: auto;
+                min-height:10px;
+            }
+
+            /* Hide the carousel text when the screen is less than 600 pixels wide */
+            @media (max-width: 600px) {
+                .carousel-caption {
+                    display: none;
+                }
+            }
+        </style>
+
+
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="images_catalogue/stab-solid.jpg" style="width:20%" alt="Image">
+                    <div class="carousel-caption">
+                        <h3>Sell $</h3>
+                        <p>Money Money.</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="images_catalogue/masque-de-plongee-avec-verres-correcteurs-pro.jpg" style="width:20%" alt="Image">
+                    <div class="carousel-caption">
+                        <h3>More Sell $</h3>
+                        <p>Lorem ipsum...</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+
 	</main>
 
 <?php
