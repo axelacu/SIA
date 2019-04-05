d3043c38d0083dadcfcaff6c2da90e8ba487619e
shirelmatti <shirel.matti@dauphine.eu>
2019-04-05 16:02:29 +0200

    Firts push shirel

---
+++ Video/Panier.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shirelmatti
+ * Date: 2019-04-04
+ * Time: 12:25
+ */
+
+class Panier
+{
+
+    private $products;
+
+
+    public function getProducts(){
+        return $this->products;
+    }
+
+    public function setProducts($products){
+        $this->products=$products;
+    }
+
+    public function addProduct(Product $product){
+        $this->products[]=$product;
+        return $this;
+    }
+
+    public function getPrice(): float { return array_reduce($this->getProducts(),
+        function ($total,Product $product){
+            return $product->getprice()+$total;
+        },0);
+
+    }
+
+    public function getVatPrice($rate):float {
+        return round($this->getPrice()*$rate*100)/100;
+    }
+
+
+    public static function fake(): Panier{
+        $product = new Product();
+        $product->setPrice(1.21);
+        $product->setName('Produit1');
+        $productsfake[0]=$product;
+        $product = new Product();
+        $product->setPrice(23);
+        $product->setName('Produit2');
+        $productsfake[1]=$product;
+        $product = new Product();
+        $product->setPrice(56);
+        $product->setName('Produit3');
+        $productsfake[2]=$product;
+
+        $return = new Panier();
+        $return->setProducts($productsfake);
+
+        return $return;
+    }
+
+}
---
+++ Video/payment.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shirelmatti
+ * Date: 2019-04-03
+ * Time: 19:24
+ */
+require 'vendor/autoload.php';
+require 'Product.php';
+require 'Panier.php';
+
+$ids= require('paypal.php');
+$basket = Panier::fake();
+
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        $ids['id'], $ids['secret']
+    )
+);
+
+
+
+$list = new \PayPal\Api\ItemList();
+
+foreach ($basket->getProducts() as $product){
+    $item=new \PayPal\Api\Item();
+    $item->setName($product->getName());
+    $item->setPrice($product->getPrice());
+    $item->setCurrency('EUR');
+    $item->setQuantity(1);
+    $list->addItem($item);
+}
+
+$details = new \PayPal\Api\Details();
+$amount = new \PayPal\Api\Amount();
+$amount->setTotal($basket->getPrice());
+$amount->setCurrency('EUR');
+$amount->setDetails($details);
+
+$transaction = new \PayPal\Api\Transaction();
+$transaction->setItemList($list);
+$transaction->setDescription('Achat sur le site Airblio');
+$transaction->setAmount($amount);
+$transaction->setCustom('demo1');
+
+$payment = new \PayPal\Api\Payment();
+$payment->setIntent('sale');
+$redirectUrls=new \PayPal\Api\RedirectUrls();
+$redirectUrls->setReturnUrl('http://localhost:8888/AIRBLIO/SIA/Video/pay.php');
+$redirectUrls->setCancelUrl('http://localhost:8888/index.php');
+$payment->setRedirectUrls($redirectUrls);
+
+$payment->setPayer((new \PayPal\Api\Payer())->setPaymentMethod('paypal'));
+
+$payment->setTransactions([$transaction]);
+
+
+
+
+try {
+    $payment->create($apiContext);
+   echo json_encode([
+       'id'=> $payment->getId()
+   ]);
+   exit;
+
+}catch(\PayPal\Exception\PayPalConnectionException $e){
+    echo(var_dump(json_decode($e->getData())));
+}
+
