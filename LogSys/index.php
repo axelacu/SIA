86a531a8866f2fb75516f1240d8938ab8282668f
houzetorlane <orlanehzt@gmail.com>
2019-04-07 16:52:26 +0200

    Ajout du fichier service.php et petite modification du carroussel

--- LogSys/index.php
+++ LogSys/index.php
@@ -10,6 +10,8 @@
             }
         ?>
 
+        <!-- Debut du carroussel -->
+
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -25,49 +27,50 @@
 
             /* Hide the carousel text when the screen is less than 600 pixels wide */
             @media (max-width: 600px) {
-                .carousel-caption {
+                .carousel slide {
                     display: none;
                 }
             }
         </style>
 
 
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-            </ol>
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img src="images/bouteilles_de_gaz.jpg" style="width:45%" alt="Image">
-                    <div class="carousel-caption">
-                        <h3>Sell $</h3>
-                        <p>Money Money.</p>
+        <div class="container" style="margin-bottom: 30px;">
+            <h2>Bienvenue !</h2>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <!--<li data-target="#myCarousel" data-slide-to="2"></li>-->
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="images/plongeur.jpg" alt="Plongeur" style="width:100%;">
                     </div>
+
+                    <div class="item">
+                        <img src="images/bouteilles_de_gaz.jpg" alt="Bouteilles de gaz" style="width:100%;">
+                    </div>
+
                 </div>
 
-                <div class="item">
-                    <img src="images/plongeur.jpg" style="width:45%" alt="Image">
-                    <div class="carousel-caption">
-                        <h3>More Sell $</h3>
-                        <p>Lorem ipsum...</p>
-                    </div>
-                </div>
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
+
+
+        <!-- Fin du carroussel -->
 
 
 	</main>
@@ -75,3 +78,43 @@
 <?php
 require 'footer.php';
 ?>
+
+
+<!--
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!- Indicators --
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+
+    <!- Wrapper for slides --
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="images/bouteilles_de_gaz.jpg" style="width:45%" alt="Image">
+            <div class="carousel-caption">
+                <h3>Sell $</h3>
+                <p>Money Money.</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="images/plongeur.jpg" style="width:45%" alt="Image">
+            <div class="carousel-caption">
+                <h3>More Sell $</h3>
+                <p>Lorem ipsum...</p>
+            </div>
+        </div>
+    </div>
+
+    <!- Left and right controls --
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+-->
---
+++ LogSys/services.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: houzetorlane
+ * Date: 2019-04-07
+ * Time: 16:30
+ */
+
+    require 'header.php';
+?>
+
+<main>
+    <section>
+        <div>
+            Hello!
+        </div>
+    </section>
+</main>
+
+<?php
+    require 'footer.php';
+?>
