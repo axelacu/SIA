b5c64eaa76f0f0f5f080a3cb8dce6c78f42ca818
shirelmatti <shirel.matti@dauphine.eu>
2019-04-07 17:03:43 +0200

    database basket

---
+++ LogSys/includes/basket.inc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shirelmatti
+ * Date: 2019-04-07
+ * Time: 14:46
+ */
+
+require "dbh.inc.php";
+//on récupere les enregistrements qui n'on pas encore été validé
+$req= "SELECT ID_USERBASKET FROM USERBASKET WHERE ID_USER=".$_SESSION['USER_ID']." AND ACCEPTED=0";
+$result = mysqli_query($conn,$req);
+echo '<meta name="viewport" content="width=device-width, initial-scale=1">
+       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-highway.css">';
+
+
