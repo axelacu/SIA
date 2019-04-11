5e8431d64b546c95548426c09eb8331f3ddfa191
houzetorlane <orlanehzt@gmail.com>
2019-04-11 12:55:01 +0200

    Amélioration de la page -affichage produit-

--- LogSys/includes/display_product.inc.php
+++ LogSys/includes/display_product.inc.php
@@ -6,22 +6,20 @@
  * Time: 18:10
  */
 
-//include 'header.php';
-
 
 if (isset($_GET['label']) && isset($_GET['type_offre']) && isset($_GET['file_name']) && isset($_GET['description']) && isset($_GET['prix'])) {
 
 
     $attributes = array($_GET['label'], $_GET['type_offre'], $_GET['file_name'], $_GET['description'], $_GET['prix']);
 
-    //header("Location: /SIA/LogSys/display_product?login=success");
-
     echo '
         <section class="blanc">
             <link rel="stylesheet" href="style.css" />
                <div>
                     <h1>'.$attributes[0].'</h1>
-                    
+                    <img class="w3-border w3-margin-top" src="' . $attributes[2] . '" alt="' . $attributes[2] . '" style="width:40%">
+                    <p> ' . $attributes[3] . '</p>
+                    <h5>' . $attributes[4] . '€' . '</h5> 
                     <a href="#.php">
                         <button type="submit" name="add_basket" class="w3-button w3-black">Add to basket<i class="fa fa-shopping-cart"></i></button>
                     </a>
