7b42f5699fd566d4672e4450bc86f4518e2d6420
houzetorlane <orlanehzt@gmail.com>
2019-04-16 18:11:14 +0200

    updapte_product

--- LogSys/includes/update_product.inc.php
+++ LogSys/includes/update_product.inc.php
@@ -6,3 +6,37 @@
  * Time: 16:18
  */
 
+require 'dbh.inc.php';
+
+//On récupère les produits de la base de données
+$dir = 'images_catalogue';
+
+
+$req = "SELECT DISTINCT OFFRE.id_offre, OFFRE.file_name, OFFRE.label, OFFRE.quantite, OFFRE.type_offre FROM OFFRE WHERE type_offre=0";
+
+$result = mysqli_query($conn,$req);
+
+if($result) {
+    while ($donnees = mysqli_fetch_row($result)) {
+        echo '
+                <tr>
+                    <td>'. $donnees[0] .'</td>
+                    <td style="width: 20%"> <div class="w3-cell">
+                        <img src="./images_catalogue/' . $donnees[1] . '" style="width:40%">
+                    </td>
+                    <td style="text-align: center">' . $donnees[2] . '</td>
+                    <td style="text-align: center">' . $donnees[3] . '</td>
+                    <td style="text-align: right; margin: 10px 10px; display: flex">
+                        <input style="flex: 1; padding: 20px; " type="number" min="-200" max="200" step="5" name="qty" id="quantity" value=0><br/>
+                        <form action=<?php update(); ?>>
+                            <button style="flex: 2" class="button" style="color: white; background: black ">Update stock</button>
+                        </form>
+                    </td>
+                </tr>
+        ';
+    }
+}
+
+function update(){
+
+}
--- LogSys/update_product.php
+++ LogSys/update_product.php
@@ -8,54 +8,35 @@
 
 require 'header.php';
 
-
-
-
 ?>
 
 
 <main>
-
-    <section class="blanc">
-        <div class="inscription">
-            <h1>Update product</h1>
+    <section>
+        <div id="commands">
+            <section class="blanc" id="demand">
+                <div class="inscription">
+                    <h1>Update products</h1>
+                </div>
+                <div class="w3-container">
+                    <table class="w3-table w3-bordered">
+                        <tr>
+                            <th><label> <b>Reference</b></label></th>
+                            <th><label> <b>Article</b></label></th>
+                            <th style="text-align: center"><label> <b>Label</b></label></th>
+                            <th style="text-align: center"><label> <b>En stock</b></label></th>
+                            <th style="width: 10%"><label> <b></b></label></th>
+                        </tr>
+                        <?php
+                            include ('includes/update_product.inc.php');
+                        ?>
+                    </table>
+                </div>
+            </section>
         </div>
 
-        <form action="includes/update_product.inc.php" method="post" enctype="multipart/form-data">
-            <div id="signup">
 
-                <label for="type"><b>Type</b></label><br/>
-                <input type="radio" name="choix" value="Product" id="product" onClick="masquer_div('a_masquer');">Product<br>
-                <input type="radio" name="choix" value="Service" id="serv" onClick="masquer_div('a_masquer');">Service<br>
-
-                <label for="label"><b>Name</b></label>
-                <input type="text" name="label" placeholder="Name">
-
-                <label for="description"><b>Description</b></label>
-                <textarea name="desc"  id="description" placeholder="Description of the product"></textarea>
-
-                <label for="price"><b>Price</b></label>
-                <input type="text" name="price" placeholder="Price">
-
-                <div id="a_masquer">
-                    <label for="quantity"><b>Quantity</b></label><br/>
-                    <input type="number" min="0" name="qty" id="quantity" value=1><br/>
-                </div>
-
-                <label for="picture">Fichier (tous formats | max. 1 Mo) :</label>
-                <input type="file" name="picture" />
-
-                <br/>
-                <br/>
-
-                <div class="clearfix">
-                    <button type="button" class="cancelbtn">Cancel</button>
-                    <button type="submit" name="register-submit" class="signupbtn">Register</button>
-                </div>
-            </div>
-        </form>
     </section>
-
 </main>
 
 
