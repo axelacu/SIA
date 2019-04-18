4a11d58998ffb0cfef2c9cb5af86507a7c9ff834
shirelmatti <shirel.matti@dauphine.eu>
2019-04-18 13:34:34 +0200

    repar date

--- LogSys/includes/buttonPayment.inc.php
+++ LogSys/includes/buttonPayment.inc.php
@@ -8,8 +8,10 @@
 
 
 require 'dbh.inc.php';
-$req='SELECT * FROM COMMAND C, DEMAND D WHERE D.ID_USER='.$_SESSION['USER_ID'].' AND C.VALIDATE_COMMAND = 1 AND C.PAYMENT = 0 AND D.ID_DEMAND = C.ID_DEMAND;';
+$req='SELECT Dev.ID_DEV,D.QUANTITE, I.lABEL, I.PRIX, C.DATE_START, C.DATE_END, C.DATE_VALIDATION, E.EMPLOYEE_NAME, E.EMPLOYEE_LAST_NAME   FROM COMMAND C, DEMAND D, DEVIS Dev,OFFRE I,EMPLOYEE E WHERE DEV.ID_EMPLOYEE = E.ID_EMPLOYEE AND I.ID_OFFRE = D.ID_OFFRE AND Dev.ID_DEVIS = C.ID_DEVIS AND D.ID_USER='.$_SESSION['USER_ID'].' AND C.VALIDATE_COMMAND = 1 AND C.PAYMENT = 0 AND D.ID_DEMAND = C.ID_DEMAND;';
 $result = mysqli_query($conn,$req);
+
+
 
 if(mysqli_num_rows($result) != 0) {
     echo'
@@ -41,6 +43,16 @@ if(mysqli_num_rows($result) != 0) {
                             </script>
                         </div>
                     </div>
+         <div class="w3-container" >
+            <div class="w3-right">
+            
+            
+            <form class="w3-bar-item" action="includes/generate_textFile?numproduit='.mysqli_num_rows($result).'" method="post" >
+                  <button type="submit" class="button"  style="color: white; width:200px; background: black; margin-top: 10px ;"  >Voir mon devis</button>
+            </form> 
+
+            </div>
+        </div>
     ';
 } else {
 
