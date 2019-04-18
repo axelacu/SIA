3973d6df867d3dfd73d4d75d37880a40d312d8e9
BouadiAnia <ania95@outlook.fr>
2019-04-18 11:42:47 +0200

    BD

--- LogSys/map_monde.php
+++ LogSys/map_monde.php
@@ -1,15 +1,26 @@
+
 <?php
+include 'header.php';
+?>
+<main>
+    <?php
+    if(!isset($_SESSION["USER_NAME"])){
+        echo '<p>Vous êtes déconnecté</p>';
+    }else{
+        echo '<p>Vous êtes connecté</p>';
+    }
+    ?>
+</main>
 
- require 'header.php';
- ?>
-
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
 
 <link rel='stylesheet' href='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.css'>
 
 <link rel="stylesheet" href="style_mapmonde.css">
 
-    <main>
+
+
+    <div class="principale">
             <!-- filtering of crops -->
             <nav class='menu-ui'>
                 <a href='#' class='activenew' data-filter='all'>Voir tout</a>
@@ -31,15 +42,15 @@
 
             <!-- map itself -->
             <div id='map' class="map"></div>
-            <script src='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js'></script>
+    </div>
+
+
+
+
+<script src='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js'></script>
             <script src='https://code.jquery.com/jquery-1.11.0.min.js'></script>
 
 
 
             <script  src="includes/mapmonde.js"></script>
 
-    </main>
-
-<?php
-    require 'footer.php';
-?>
